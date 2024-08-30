169dc72d57ad1b7a3cf9310cf7a836fb3536de14
Marek Sterzik <marek.sterzik@gmail.com>
2024-08-30 18:23:16 +0200

    add temporarily eeg2d export

--- src/webpack.js
+++ src/webpack.js
@@ -1,4 +1,5 @@
 
 import {Mechanics, Body} from "./mech2d.js"
-
+import {Vector, Point, Angle, Transformation, StringConvertor} from "eeg2d"
 window.mech2d = {Mechanics, Body}
+window.eeg2d = {Vector, Point, Angle, Transformation, StringConvertor}
